9cf8422461149925bc6aaa96fdceda1865bb8064
Albert <albert.gomes@liferay.com>
2024-07-05 21:19:27 -0300

    Create buildRest structure

---
+++ modules/management/management-impl/src/main/java/management/internal/jaxrs/application/ManagementApplication.java
@@ -0,0 +1,23 @@
+package management.internal.jaxrs.application;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author me
+ * @generated
+ */
+@Component(
+	property = {
+		"liferay.jackson=false", "osgi.jaxrs.application.base=/management",
+		"osgi.jaxrs.extension.select=(osgi.jaxrs.name=Liferay.Vulcan)",
+		"osgi.jaxrs.name=Management"
+	},
+	service = Application.class
+)
+@Generated("")
+public class ManagementApplication extends Application {
+}
