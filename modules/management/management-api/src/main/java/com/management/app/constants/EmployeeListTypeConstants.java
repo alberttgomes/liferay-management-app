9f930d5eab647022b2ff74ea5e574e7733014733
Albert <albert.gomes@liferay.com>
2024-09-14 19:30:37 -0300

    Create Employee list type constants

---
+++ modules/management/management-api/src/main/java/com/management/app/constants/EmployeeListTypeConstants.java
@@ -0,0 +1,21 @@
+package com.management.app.constants;
+
+/**
+ * @author Albert Cabral
+ */
+public class EmployeeListTypeConstants {
+
+    public static final String EMPLOYEE = "employee";
+
+    public static final String EMPLOYEE_ENTRY_DEPARTMENT =
+            "employee-entry-department";
+
+    public static final String EMPLOYEE_ENTRY_MANAGERS =
+            "employee-entry-managers";
+
+    public static final String EMPLOYEE_ENTRY_POSITION =
+            "employee-entry-position";
+
+    public static final String MANAGER = "manager";
+
+}
