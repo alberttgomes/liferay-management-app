150b474989f19d1e04db6cdd97bbc78005f0013e
Albert <albert.gomes@liferay.com>
2024-09-15 21:14:09 -0300

    Create new keys on ManagementPortletKeys

--- modules/management/management-web/src/main/java/management/web/constants/ManagementPortletKeys.java
+++ modules/management/management-web/src/main/java/management/web/constants/ManagementPortletKeys.java
@@ -5,12 +5,16 @@ package management.web.constants;
  */
 public class ManagementPortletKeys {
 
-	public static final String MANAGEMENT_WEB =
-		"management_web_ManagementPortlet";
+	public static final String EMPLOYEE_DISPLAY = "EMPLOYEE_DISPLAY";
+
+	public static final String EMPLOYEE_PATH = "/employees";
 
 	public static final String MANAGEMENT_DISPLAY_CONTEXT =
 			"MANAGEMENT_DISPLAY_CONTEXT";
 
-	public static final String EMPLOYEE_DISPLAY = "EMPLOYEE_DISPLAY";
+	public static final String MANAGER_PATH = "/managers";
+
+	public static final String MANAGEMENT_WEB =
+			"management_web_ManagementPortlet";
 
 }
