6aa2e836a1ff384df24d7052f23c69be266c8d8f
Albert <albert.gomes@liferay.com>
2024-09-16 21:08:01 -0300

    Remove RandomStringUtil to generate randoms strings to default password

--- modules/management/management-api/src/main/java/com/management/app/util/ManagementUtil.java
+++ modules/management/management-api/src/main/java/com/management/app/util/ManagementUtil.java
@@ -1,6 +1,6 @@
 package com.management.app.util;
 
-import org.apache.commons.lang3.RandomStringUtils;
+import java.util.Random;
 
 /**
  * @author Albert Cabral
@@ -8,12 +8,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class ManagementUtil {
 
     public static String generateRandomPassword() {
-        int length = 32;
-        boolean useLetters = true;
-        boolean useNumbers = true;
-
-        return RandomStringUtils.random(
-                length, useLetters, useNumbers);
+        return new Random().doubles().toString();
 
     }
 
