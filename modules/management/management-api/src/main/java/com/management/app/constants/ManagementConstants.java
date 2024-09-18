b5676b95b69869deca87ae31e638c2dd2ee8baa3
Albert <albert.gomes@liferay.com>
2024-09-17 22:35:51 -0300

    Create MangementConstants class

---
+++ modules/management/management-api/src/main/java/com/management/app/constants/ManagementConstants.java
@@ -0,0 +1,12 @@
+package com.management.app.constants;
+
+/**
+ * @author Albert Cabral
+ */
+public class ManagementConstants {
+
+    public static final String MANAGEMENT_DOMAIN =
+            "management.com";
+
+
+}
