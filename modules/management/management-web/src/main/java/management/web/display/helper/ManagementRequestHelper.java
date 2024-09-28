f141fe1af72f562c8460d71581e3d33cc3d41925
Albert <albert.gomes@liferay.com>
2024-09-28 17:16:58 -0300

    Feat employee frontend data set tag lib

---
+++ modules/management/management-web/src/main/java/management/web/display/helper/ManagementRequestHelper.java
@@ -0,0 +1,16 @@
+package management.web.display.helper;
+
+import com.liferay.portal.kernel.display.context.helper.BaseRequestHelper;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Albert Cabral
+ */
+public class ManagementRequestHelper extends BaseRequestHelper {
+
+    public ManagementRequestHelper(HttpServletRequest httpServletRequest) {
+        super(httpServletRequest);
+    }
+
+}
