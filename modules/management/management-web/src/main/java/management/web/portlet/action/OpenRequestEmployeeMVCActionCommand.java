7ed3ab366d5442e6dfcf3d47b3763e0146b308c1
Albert <albert.gomes@liferay.com>
2024-10-02 19:22:36 -0300

    Update actions and renderes

--- modules/management/management-web/src/main/java/management/web/portlet/action/OpenRequestEmployeeMVCActionCommand.java
+++ modules/management/management-web/src/main/java/management/web/portlet/action/OpenRequestEmployeeMVCActionCommand.java
@@ -1,15 +1,29 @@
 package management.web.portlet.action;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
 
+import com.management.app.model.Employee;
+import com.management.app.service.EmployeeLocalService;
+
+import java.io.IOException;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletException;
+import javax.servlet.http.HttpServletRequest;
 
+import management.web.constants.EmployeeRequestConstant;
 import management.web.constants.ManagementPortletKeys;
+import management.web.display.context.ManagementDisplayContext;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Albert Cabral
@@ -26,7 +40,49 @@ public class OpenRequestEmployeeMVCActionCommand extends BaseMVCActionCommand {
     @Override
     public void doProcessAction(
             ActionRequest actionRequest, ActionResponse actionResponse)
-        throws PortletException {
+        throws PortalException, IOException {
 
+        String description = ParamUtil.getString(actionRequest, "description");
+        long employeeId = ParamUtil.getInteger(actionRequest, "employeeId");
+        int newLevel = ParamUtil.getInteger(actionRequest, "newLevel");
+        String newPosition = ParamUtil.getString(actionRequest, "newPosition");
+        String reason = ParamUtil.getString(actionRequest, "reason");
+        long type = Long.parseLong(ParamUtil.getString(actionRequest, "type"));
+
+        if (!(type == EmployeeRequestConstant.PROMOTION)) {
+            return;
+        }
+
+        Employee employee = _employeeLocalService.getEmployee(employeeId);
+
+        if (employee == null) {
+            return;
+        }
+
+        HttpServletRequest httpServletRequest =
+                _portal.getHttpServletRequest(actionRequest);
+
+        ThemeDisplay themeDisplay =
+                (ThemeDisplay)httpServletRequest.getAttribute(
+                        WebKeys.THEME_DISPLAY);
+
+        employee = _employeeLocalService.employeePromoting(
+                newPosition, themeDisplay.getUserId(), employee.getDepartment(),
+                newLevel, employeeId, true);
+
+        actionRequest.setAttribute(
+                "employeeId", employee.getEmployeeId());
+
+        sendRedirect(actionRequest, actionResponse);
     }
+
+    @Reference
+    private EmployeeLocalService _employeeLocalService;
+
+    @Reference
+    private UserLocalService _userLocalService;
+
+    @Reference
+    private Portal _portal;
+
 }
